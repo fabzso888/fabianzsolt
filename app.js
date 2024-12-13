0c88e8be0547814b292f46377ef596f564c73c30
Fábián Zsolt <118449787+fabzso888@users.noreply.github.com>
2024-12-13 11:23:25 +0100

    Add files via upload
    2024.12.13.

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const btn =document.createElement('button')
 btn.onclick=function(){
-    alert("ez egy natív kód");
+    alert("Ez egy natív kód");
 }
-btn.innerHTML="Nativ"
+btn.innerHTML="Natív gomb"
 document.getElementById("nativ-button-container").appendChild(btn);
  
 const gomb = React.createElement("button",
 {
     onclick:function()
     {
-        alert("ez egy natív kód")
+        alert("Ez egy react kód")
     },
 },
 "React gomb"
 )
 ReactDOM.render(gomb,documen.getElementById("react-button-container"));
+//get, post, delete
+//3 funkció és már értékelhető a feladat
